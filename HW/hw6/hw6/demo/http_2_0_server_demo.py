1306bd461c41c8e78b553dfd69d75e783aecf261
James24029775 <heartstonejames@gmail.com>
2023-05-15 22:43:48 +0800

    update

--- HW/hw6/hw6/demo/http_2_0_server_demo.py
+++ HW/hw6/hw6/demo/http_2_0_server_demo.py
@@ -1,5 +1,8 @@
 import sys
-sys.path.append("/home/james/NSC/HW/hw6/hw6")
+import os
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
 from my.http_2_0_server import HTTPServer
 
 if __name__ == '__main__':
